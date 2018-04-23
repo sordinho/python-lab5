2ad8eb2473833f0f4b44a12590a98367393425d6
Davide Sordi <davi.sordi96@gmail.com>
2018-04-23 17:42:05 +0200

    working exercise

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import taskDB
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+	# welcome page index.html
+	return render_template("index.html", tasks=taskDB.read_from_db())
 
+
+@app.route('/insert-task', methods=['POST'])
+def insert_new_task():
+	# retrieve the task to insert in db
+	task_to_insert = request.form['new_task']
+
+	# print(task_to_insert)
+
+	# insert into DB
+	taskDB.insert_in_db(task_to_insert)
+
+	# go back the the updated home
+	return render_template("index.html", tasks=taskDB.read_from_db())
+
+@app.route('/delete-task/<id>') #id will bw the parameter to pass at delete_task function
+def delete_task(id):
+	taskDB.remove_from_db(id)
+
+	# go back the the updated home
+	return render_template("index.html", tasks=taskDB.read_from_db())
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
---
+++ taskDB.py
@@ -0,0 +1,72 @@
+# Created by Davide Sordi in 23/04/2018 at 16.46
+
+import pymysql
+
+
+def read_from_db():
+	"""
+	Reading task list from a DB
+	:return: a dictionary of tasks
+	"""
+	# prepare the query for reading from DB
+	query = "SELECT * FROM tasks"
+
+	# connectione to database
+	connection = pymysql.connect(user="root", password="sysadmin", host="localhost", database="todolist")
+
+	# get a cursor
+	cursor = connection.cursor()
+
+	# execute query
+	cursor.execute(query)
+
+	# fetch result from query
+	results = cursor.fetchall()
+
+	# close cursor and connection
+	cursor.close()
+	connection.close()
+
+	return results
+
+
+def remove_from_db(id_task_to_rem):
+	"""
+	function for deleting task from DB
+	:param task: task to delete in DB
+	"""
+	# delete query
+	query = "DELETE FROM tasks WHERE id_task=(%s)"
+
+	# connection to database
+	connection = pymysql.connect(user="root", password="sysadmin", host="localhost", database="todolist")
+	# get a cursor
+	cursor = connection.cursor()
+
+	# execute query
+	cursor.execute(query, (id_task_to_rem,))
+	# commit on DB
+	connection.commit()
+	# close cursor and connection
+	cursor.close()
+	connection.close()
+
+
+def insert_in_db(task):
+	"""
+	insert a new task in DB
+	:param task: the task to insert
+	"""
+	# insert query
+	query = "INSERT INTO tasks (todo) VALUES (%s)"
+	# connection to database
+	connection = pymysql.connect(user="root", password="sysadmin", host="localhost", database="todolist")
+	# get a cursor
+	cursor = connection.cursor()
+	# execute query
+	cursor.execute(query, (task,))
+	# commit on DB
+	connection.commit()
+	# close cursor and connection
+	cursor.close()
+	connection.close()
